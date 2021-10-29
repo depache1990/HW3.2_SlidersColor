b35b7312ce0effaa010cca5ddd2b2e45874f67e2
parkurru@mail.ru <parkurru@mail.ru>
2021-10-29 10:40:54 +0300

    Initial Commit

---
+++ HW3.2_SlidersColor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.2_SlidersColor
+//
+//  Created by Anton Duplin on 29/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3.2_SlidersColor/HW3_2_SlidersColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_2_SlidersColorApp.swift
+//  HW3.2_SlidersColor
+//
+//  Created by Anton Duplin on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HW3_2_SlidersColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
